ea95f8949f3efc08ebcd982f3ec15ff12fbd8b77
Andrew Grathwohl <andrew@grathwohl.me>
2020-09-26 14:18:54 -0400

    improve `minterpolate` filter config

--- video.js
+++ video.js
@@ -2,29 +2,28 @@ const ffmpeg = require('fluent-ffmpeg')
 const path = require('path')
 
 async function constructFFmpegCommand(ratios, files, fps, opts) {
-    const ffmpegCmd = ffmpeg(files.video).input(files.audio)
+    const ffmpegCmd = ffmpeg(files.video).inputOptions([ '-hwaccel', 'nvdec' ]).input(files.audio)
     ffmpegCmd.outputOptions([
         '-map 0:v:0',
         '-map 1:a:0',
-        `-r ${(fps * ratios.durRatio > 60 ? 60 : fps * ratios.durRatio)}`,
+        // `-r ${(fps * ratios.durRatio > 60 ? 60 : fps * ratios.durRatio)}`,
         '-c:a flac',
         '-c:v', `${opts.gpu ? 'h264_nvenc' : 'libx264'}`,
-        '-b:v 20M',
-        '-maxrate', '40M',
-        '-bufsize', '80M',
-        '-crf', '18',
-        '-crf_max', '34',
+        '-b:v 4M',
+        '-maxrate', '4M',
+        '-bufsize', '8M',
+        /*'-crf', '18',
+        '-crf_max', '34',*/
         '-preset:v', `${opts.gpu ? 'fast' : 'ultrafast'}`,
         '-vsync -1',
         '-af aresample=async=1',
-        '-threads 8',
-        '-movflags', '+faststart+negative_cts_offsets',
-        '-tune:v', 'animation',
-        '-flags', '+global_header',
+        //'-movflags', '+faststart+negative_cts_offsets',
+        //'-tune:v', 'animation',
+        //'-flags', '+global_header',
         '-strict', '-2',
         '-f', `${opts.container || 'matroska'}`
     ]).videoFilters([
-        `setpts=${ratios.ptsRatio}*PTS`//,minterpolate=fps=50`,
+        `setpts=${ratios.ptsRatio}*PTS,minterpolate=fps=60:mi_mode=mci:mc_mode=aobmc:vsbmc=1:scd=none`,
     ])
     .output(`${files.video}_noGlass.mkv`)
     .on('start', (cmd) => console.log(cmd))
